a0fe4b78aa2ca28540e03dd2fe18d4abaa2705e8
Steven Williams <gev3@cdc.gov>
2016-02-09 14:11:11 -0500

    Add basic Epi Info-dependent functionality

--- ERHMS.Sandbox/App.xaml.cs
+++ ERHMS.Sandbox/App.xaml.cs
@@ -1,6 +1,20 @@
-﻿using System.Windows;
+﻿using Epi;
+using System.IO;
+using System.Windows;
 
 namespace ERHMS.Sandbox
 {
-    public partial class App : Application { }
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (!File.Exists(Configuration.DefaultConfigurationPath))
+            {
+                Configuration configuration = Configuration.CreateDefaultConfiguration();
+                Configuration.Save(configuration);
+            }
+            Configuration.Load(Configuration.DefaultConfigurationPath);
+        }
+    }
 }
--- ERHMS.Sandbox/MainWindow.xaml.cs
+++ ERHMS.Sandbox/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Epi;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Documents;
 
 namespace ERHMS.Sandbox
 {
@@ -7,6 +10,29 @@ namespace ERHMS.Sandbox
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Epi Info 7 Project Files (*.prj)|*.prj";
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                Views.Inlines.Clear();
+                using (Project project = new Project(dialog.FileName))
+                {
+                    foreach (View view in project.Views)
+                    {
+                        Views.Inlines.Add(new Run(view.Name));
+                        Views.Inlines.Add(new LineBreak());
+                    }
+                }
+            }
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
