ec6f5f08dea8a0a86e603c2586d53f2bec64af8a
Steven Williams <gev3@cdc.gov>
2016-02-11 16:00:44 -0500

    Change company/publisher name

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ERHMS Info Manager")]
-[assembly: AssemblyCompany("The National Institute for Occupational Safety and Health (NIOSH)")]
+[assembly: AssemblyCompany("NIOSH")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
