91e68026e2c2e4d50f54d8ce3719c0e305a58f39
Steven Williams <gev3@cdc.gov>
2016-02-11 14:53:43 -0500

    Prevent duplicate entries in application manifest

---
+++ ERHMS.Build/FindFilesInList.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ERHMS.Build
+{
+    public class FindFilesInList : Task
+    {
+        [Required]
+        public ITaskItem[] Items { get; set; }
+
+        [Required]
+        public ITaskItem[] ItemsToFind { get; set; }
+
+        [Output]
+        public ITaskItem[] FoundItems { get; set; }
+
+        public override bool Execute()
+        {
+            ICollection<string> pathsToFind = ItemsToFind.Select(item => new FileInfo(item.ItemSpec).FullName).ToList();
+            ICollection<ITaskItem> foundItemsList = new List<ITaskItem>();
+            foreach (ITaskItem item in Items)
+            {
+                FileInfo file = new FileInfo(item.ItemSpec);
+                if (pathsToFind.Contains(file.FullName))
+                {
+                    foundItemsList.Add(item);
+                }
+            }
+            FoundItems = foundItemsList.ToArray();
+            return true;
+        }
+    }
+}
