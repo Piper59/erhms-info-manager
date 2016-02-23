8e6184bbefdcaad1bd0094b9fa475e950fa18ee7
Steven Williams <gev3@cdc.gov>
2016-02-23 14:20:46 -0500

    Enhance configuration extensions

--- ERHMS.EpiInfo/ConfigurationExtensions.cs
+++ ERHMS.EpiInfo/ConfigurationExtensions.cs
@@ -5,14 +5,26 @@ namespace ERHMS.EpiInfo
 {
     public static class ConfigurationExtensions
     {
+        public static Configuration Current { get; private set; }
+
+        public static void LoadConfiguration(string path)
+        {
+            Configuration.Load(path);
+            Current = Configuration.GetNewInstance();
+        }
+
         public static void LoadDefaultConfiguration()
         {
             if (!File.Exists(Configuration.DefaultConfigurationPath))
             {
-                Configuration configuration = Configuration.CreateDefaultConfiguration();
-                Configuration.Save(configuration);
+                Configuration.Save(Configuration.CreateDefaultConfiguration());
             }
-            Configuration.Load(Configuration.DefaultConfigurationPath);
+            LoadConfiguration(Configuration.DefaultConfigurationPath);
+        }
+
+        public static void Refresh()
+        {
+            LoadConfiguration(Current.ConfigFilePath);
         }
     }
 }
