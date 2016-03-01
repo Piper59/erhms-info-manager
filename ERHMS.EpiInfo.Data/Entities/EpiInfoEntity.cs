210d0d7a697a261440d7931732519dadadede776
Steven Williams <gev3@cdc.gov>
2016-03-01 15:19:28 -0500

    Add partial repository-based data access

---
+++ ERHMS.EpiInfo.Data/Entities/EpiInfoEntity.cs
@@ -0,0 +1,76 @@
+﻿using Epi;
+using System.Data;
+
+namespace ERHMS.EpiInfo.Data.Entities
+{
+    public abstract class EpiInfoEntity : EntityBase
+    {
+        public int? UniqueKey
+        {
+            get { return GetProperty<int?>(ColumnNames.UNIQUE_KEY); }
+            internal set { SetProperty(ColumnNames.UNIQUE_KEY, value); }
+        }
+
+        public string GlobalRecordId
+        {
+            get { return GetProperty<string>(ColumnNames.GLOBAL_RECORD_ID); }
+            internal set { SetProperty(ColumnNames.GLOBAL_RECORD_ID, value); }
+        }
+
+        public string ForeignKey
+        {
+            get { return GetProperty<string>(ColumnNames.FOREIGN_KEY); }
+            internal set { SetProperty(ColumnNames.FOREIGN_KEY, value); }
+        }
+
+        public short? RecordStatus
+        {
+            get { return GetProperty<short?>(ColumnNames.REC_STATUS); }
+            internal set { SetProperty(ColumnNames.REC_STATUS, value); }
+        }
+
+        public bool IsNew
+        {
+            get
+            {
+                int? uniqueKey;
+                if (TryGetProperty(ColumnNames.UNIQUE_KEY, out uniqueKey))
+                {
+                    return uniqueKey == null;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public bool IsDeleted
+        {
+            get
+            {
+                short? recordStatus;
+                if (TryGetProperty(ColumnNames.REC_STATUS, out recordStatus))
+                {
+                    return recordStatus.HasValue && recordStatus.Value == 0;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        protected EpiInfoEntity()
+        {
+            SetDeleted(false);
+        }
+
+        protected EpiInfoEntity(DataRow row) : base(row) { }
+
+        internal void SetDeleted(bool deleted)
+        {
+            RecordStatus = deleted ? (short)0 : (short)1;
+        }
+    }
+}
---
+++ ERHMS.EpiInfo.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using Epi;
+using Epi.Data;
+
+namespace ERHMS.EpiInfo.Data.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        public Project Project { get; private set; }
+
+        protected IDbDriver Driver
+        {
+            get { return Project.CollectedData.GetDbDriver(); }
+        }
+
+        protected RepositoryBase(Project project)
+        {
+            Project = project;
+        }
+    }
+}
