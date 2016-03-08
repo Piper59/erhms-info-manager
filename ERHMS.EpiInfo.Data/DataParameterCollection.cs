b721ce7f1632f8cb4727003fd904e36cd05c5224
Steven Williams <gev3@cdc.gov>
2016-03-08 11:31:22 -0500

    Make ad hoc query creation easier

---
+++ ERHMS.EpiInfo.Data/DataParameterCollection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ERHMS.EpiInfo.Data
+{
+    public class DataParameterCollection : IEnumerable<DataParameter>
+    {
+        private IDataDriver driver;
+        private ICollection<DataParameter> parameters;
+
+        public DataParameterCollection(IDataDriver driver)
+        {
+            this.driver = driver;
+            parameters = new List<DataParameter>();
+        }
+
+        public void Add(DataParameter parameter)
+        {
+            parameter.Name = driver.GetParameterName(parameters.Count);
+            parameters.Add(parameter);
+        }
+
+        public DataParameter AddByValue(object value)
+        {
+            DataParameter parameter = new DataParameter(driver.GetParameterName(parameters.Count), value);
+            parameters.Add(parameter);
+            return parameter;
+        }
+
+        public string Format(string format)
+        {
+            return string.Format(format, parameters.Select(parameter => parameter.Name).ToArray());
+        }
+
+        public IEnumerator<DataParameter> GetEnumerator()
+        {
+            return parameters.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)parameters).GetEnumerator();
+        }
+    }
+}
