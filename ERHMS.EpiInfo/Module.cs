1dd02bd4a262e36efc34a9b7219cf01bdebf2d54
Steven Williams <gev3@cdc.gov>
2016-03-10 10:21:27 -0500

    Move module arguments to inner class

--- ERHMS.EpiInfo/Module.cs
+++ ERHMS.EpiInfo/Module.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace ERHMS.EpiInfo
 {
@@ -22,6 +23,29 @@ namespace ERHMS.EpiInfo
 
     public static class ModuleExtensions
     {
+        public static class Arguments
+        {
+            public static string Escape(string arg)
+            {
+                return string.Format("\"{0}\"", arg.Replace("\"", "\"\""));
+            }
+
+            public static string Format(IEnumerable<string> args)
+            {
+                return string.Join(" ", args.Select(arg => Escape(arg)));
+            }
+
+            private static string Format(KeyValuePair<string, string> arg)
+            {
+                return string.Format("/{0}:{1}", arg.Key, Escape(arg.Value));
+            }
+
+            public static string Format(IDictionary<string, string> args)
+            {
+                return string.Join(" ", args.Select(arg => Format(arg)));
+            }
+        }
+
         private static FileInfo GetExecutable(this Module @this)
         {
             string fileName = string.Format("{0}.exe", @this);
