e4531e9d483a50e3e3f7c94d46157d8514eea834
Steven Williams <gev3@cdc.gov>
2016-03-15 09:01:00 -0400

    Allow use of Access 2007+ databases

--- ERHMS.EpiInfo.DataAccess/Drivers/AccessDriver.cs
+++ ERHMS.EpiInfo.DataAccess/Drivers/AccessDriver.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace ERHMS.EpiInfo.DataAccess
 {
     public class AccessDriver : DataDriverBase
     {
-        private const string Provider = "Microsoft.Jet.OLEDB.4.0";
+        private static readonly IDictionary<string, string> Providers = new Dictionary<string, string>
+        {
+            { ".mdb", "Microsoft.Jet.OLEDB.4.0" },
+            { ".accdb", "Microsoft.ACE.OLEDB.12.0" }
+        };
 
         public static AccessDriver Create(string dataSource, string password = null)
         {
             IDictionary<string, object> connectionProperties = new Dictionary<string, object>
             {
-                { "Provider", Provider },
+                { "Provider", Providers[Path.GetExtension(dataSource).ToLower()] },
                 { "Data Source", dataSource }
             };
             if (password != null)
