1dca2cd52f2ac3c21abcc298d0184f7702313899
Steven Williams <gev3@cdc.gov>
2016-03-17 08:54:43 -0400

    Use mutex to ensure single instance

--- ERHMS.Sandbox/App.xaml.cs
+++ ERHMS.Sandbox/App.xaml.cs
@@ -2,13 +2,37 @@
 using ERHMS.EpiInfo;
 using System;
 using System.IO;
+using System.Reflection;
 using System.ServiceModel;
+using System.Threading;
 using System.Windows;
 
 namespace ERHMS.Sandbox
 {
     public partial class App : Application
     {
+        public static string Name
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Name; }
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            bool created;
+            using (Mutex mutex = new Mutex(true, string.Format("Global\\{0}", Name), out created))
+            {
+                if (!created)
+                {
+                    MessageBox.Show("An instance of this application is already running.");
+                    return;
+                }
+                App app = new App();
+                MainWindow window = new MainWindow();
+                app.Run(window);
+            }
+        }
+
         private ServiceHost host;
 
         protected override void OnStartup(StartupEventArgs e)
@@ -16,7 +40,7 @@ namespace ERHMS.Sandbox
             base.OnStartup(e);
             Log.Current.Debug("Starting up");
             string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            DirectoryInfo root = new DirectoryInfo(Path.Combine(desktopPath, "ERHMS.Sandbox"));
+            DirectoryInfo root = new DirectoryInfo(Path.Combine(desktopPath, Name));
             if (!ConfigurationExtensions.TryLoad(root))
             {
                 Configuration.Save(ConfigurationExtensions.Create(root));
