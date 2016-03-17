174aaa346b6736f6a8ff8e3cea386a21ddd2650f
Steven Williams <gev3@cdc.gov>
2016-03-17 12:02:07 -0400

    Add classic analysis wrapper project

---
+++ ERHMS.EpiInfo.Analysis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ERHMS.EpiInfo.Analysis
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run();
+        }
+    }
+}
