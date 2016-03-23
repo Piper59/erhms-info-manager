1336d67e45d120de5d50745f28471feb885e4a70
Steven Williams <gev3@cdc.gov>
2016-03-23 15:06:33 -0400

    Add partial import/export functionality

---
+++ ERHMS.EpiInfo/Modules/ImportExport.cs
@@ -0,0 +1,38 @@
+﻿using Epi;
+using Epi.Enter.Forms;
+using Epi.Windows.ImportExport.Dialogs;
+
+namespace ERHMS.EpiInfo.Modules
+{
+    public static class ImportExport
+    {
+        public static void ImportFromView(View view)
+        {
+            using (ImportDataForm form = new ImportDataForm(view))
+            {
+                form.ShowDialog();
+            }
+        }
+
+        public static void ImportFromPackage(View view)
+        {
+            using (ImportEncryptedDataPackageDialog dialog = new ImportEncryptedDataPackageDialog(view))
+            {
+                dialog.ShowDialog();
+            }
+        }
+
+        public static void ImportFromWeb(View view)
+        {
+            // TODO
+        }
+
+        public static void ExportToPackage(View view)
+        {
+            using (PackageForTransportDialog dialog = new PackageForTransportDialog(view.Project.FilePath, view))
+            {
+                dialog.ShowDialog();
+            }
+        }
+    }
+}
