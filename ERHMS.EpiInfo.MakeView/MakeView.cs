385f46bd24a298cd31e38a07f59cd4e393ca97f2
Steven Williams <gev3@cdc.gov>
2016-03-31 10:38:08 -0400

    Add MakeView wrapper project

---
+++ ERHMS.EpiInfo.MakeView/MakeView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ERHMS.EpiInfo.MakeView
+{
+    public class MakeView : Wrapper
+    {
+        [STAThread]
+        internal static void Main(string[] args)
+        {
+            MainBase(typeof(MakeView), args);
+        }
+    }
+}
