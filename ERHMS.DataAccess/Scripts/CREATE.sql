530dcb7409a9101d1634c9ddf958409cba28e231
Steven Williams <gev3@cdc.gov>
2016-04-08 11:27:22 -0400

    Add script content

---
+++ ERHMS.DataAccess/Scripts/CREATE.sql
@@ -0,0 +1,38 @@
+﻿ALTER TABLE Responders
+ALTER COLUMN GlobalRecordId VARCHAR(255) NOT NULL UNIQUE;
+
+CREATE TABLE ERHMS_Incidents (
+	IncidentId VARCHAR(255) NOT NULL PRIMARY KEY,
+	Name TEXT NOT NULL,
+	Description TEXT,
+	Phase VARCHAR(255) NOT NULL,
+	StartDate DATETIME,
+	EndDateEstimate DATETIME,
+	EndDateActual DATETIME
+);
+
+CREATE TABLE ERHMS_Locations (
+	LocationId VARCHAR(255) NOT NULL PRIMARY KEY,
+	IncidentId VARCHAR(255) NOT NULL REFERENCES ERHMS_Incidents (IncidentId),
+	Description TEXT NOT NULL,
+	Latitude FLOAT,
+	Longitude FLOAT
+);
+
+CREATE TABLE ERHMS_ViewLinks (
+	ViewLinkId VARCHAR(255) NOT NULL PRIMARY KEY,
+	ViewId INTEGER NOT NULL REFERENCES metaViews (ViewId),
+	IncidentId VARCHAR(255) NOT NULL REFERENCES ERHMS_Incidents (IncidentId)
+);
+
+CREATE TABLE ERHMS_Registrations (
+	RegistrationId VARCHAR(255) NOT NULL PRIMARY KEY,
+	ResponderId VARCHAR(255) NOT NULL REFERENCES Responders (GlobalRecordId),
+	IncidentId VARCHAR(255) NOT NULL REFERENCES ERHMS_Incidents (IncidentId)
+);
+
+CREATE TABLE ERHMS_Assignments (
+	AssignmentId VARCHAR(255) NOT NULL PRIMARY KEY,
+	ViewId INTEGER NOT NULL REFERENCES metaViews (ViewId),
+	ResponderId VARCHAR(255) NOT NULL REFERENCES Responders (GlobalRecordId)
+);
