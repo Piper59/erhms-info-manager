b27190bea1f15c3cb3f315473c7f201e9996aab3
Steven Williams <gev3@cdc.gov>
2016-04-20 09:31:11 -0400

    Return caller tag on view creation

--- ERHMS.EpiInfo.Communication/ViewEventArgs.cs
+++ ERHMS.EpiInfo.Communication/ViewEventArgs.cs
@@ -1,18 +1,28 @@
-﻿namespace ERHMS.EpiInfo.Communication
+﻿using System.Text;
+
+namespace ERHMS.EpiInfo.Communication
 {
     public class ViewEventArgs : ProjectEventArgs
     {
         public string ViewName { get; private set; }
+        public string Tag { get; private set; }
 
-        public ViewEventArgs(string projectPath, string viewName)
+        public ViewEventArgs(string projectPath, string viewName, string tag = null)
             : base(projectPath)
         {
             ViewName = viewName;
+            Tag = tag;
         }
 
         public override string ToString()
         {
-            return string.Format("{0}, {1}", ProjectPath, ViewName);
+            StringBuilder result = new StringBuilder();
+            result.AppendFormat("{0}, {1}", ProjectPath, ViewName);
+            if (Tag != null)
+            {
+                result.AppendFormat(" ({0})", Tag);
+            }
+            return result.ToString();
         }
     }
 }
