994079d595eccf2dcd358f29f9ae23265c121e6c
Steven Williams <gev3@cdc.gov>
2016-04-26 10:15:39 -0400

    Add deployment functionality

--- ERHMS/Program.cs
+++ ERHMS/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ERHMS
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace ERHMS
 {
     public static class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            DirectoryInfo directory = new FileInfo(Assembly.GetExecutingAssembly().Location).Directory;
+            string executable = Path.Combine(directory.FullName, "ERHMS.Presentation.exe");
+            Process.Start(new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                WorkingDirectory = directory.FullName,
+                FileName = executable
+            });
+        }
     }
 }
