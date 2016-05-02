e39f38ed0276527b0b7e0b208fa31541ea919c97
Steven Williams <gev3@cdc.gov>
2016-05-01 22:27:02 -0400

    Add remaining responder fields

--- ERHMS.Presentation/ViewModels/ResponderDetailViewModel.cs
+++ ERHMS.Presentation/ViewModels/ResponderDetailViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using ERHMS.Domain;
 using ERHMS.Presentation.Messages;
+using ERHMS.Utility;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ERHMS.Presentation.ViewModels
 {
     public class ResponderDetailViewModel : DocumentViewModel
     {
         public Responder Responder { get; private set; }
+        public ICollection<string> Prefixes { get; private set; }
+        public ICollection<string> Suffixes { get; private set; }
+        public ICollection<string> Genders { get; private set; }
+        public ICollection<string> States { get; private set; }
 
         public RelayCommand SaveCommand { get; private set; }
 
@@ -15,6 +22,22 @@ namespace ERHMS.Presentation.ViewModels
         {
             Responder = responder;
             UpdateTitle();
+            Prefixes = DataContext.Prefixes
+                .Select()
+                .Prepend("")
+                .ToList();
+            Suffixes = DataContext.Suffixes
+                .Select()
+                .Prepend("")
+                .ToList();
+            Genders = DataContext.Genders
+                .Select()
+                .Prepend("")
+                .ToList();
+            States = DataContext.States
+                .Select()
+                .Prepend("")
+                .ToList();
             SaveCommand = new RelayCommand(Save);
         }
 
