dd0ecfcaaccc7c762d6293673a9e3c8971bc630b
Steven Williams <gev3@cdc.gov>
2016-05-08 13:21:14 -0400

    Restyle form grids

---
+++ ERHMS.Presentation/Controls/FormGrid.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ERHMS.Presentation.Controls
+{
+    public class FormGrid : Grid
+    {
+        public static readonly GridLength LabelWidth = new GridLength(150.0);
+
+        public FormGrid()
+        {
+            ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = LabelWidth
+            });
+            ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(1.0, GridUnitType.Star)
+            });
+        }
+    }
+}
