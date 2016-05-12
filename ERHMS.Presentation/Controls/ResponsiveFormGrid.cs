7ae09b7b0917790f54af5d95d3901d61b262ef60
Steven Williams <gev3@cdc.gov>
2016-05-12 17:23:42 -0400

    Add responsive form grid

---
+++ ERHMS.Presentation/Controls/ResponsiveFormGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ERHMS.Presentation.Controls
+{
+    public class ResponsiveFormGrid : Grid
+    {
+        public ResponsiveFormGrid()
+        {
+            ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(1.0, GridUnitType.Auto)
+            });
+            ColumnDefinitions.Add(new ColumnDefinition
+            {
+                Width = new GridLength(1.0, GridUnitType.Star)
+            });
+        }
+    }
+}
