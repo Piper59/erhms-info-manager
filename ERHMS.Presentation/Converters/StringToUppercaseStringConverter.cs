4a4ba725244fe3c60c8bb3d654f307f7a7a42db4
Steven Williams <gev3@cdc.gov>
2016-05-13 12:13:20 -0400

    Rename class

--- ERHMS.Presentation/Converters/StringToUppercaseStringConverter.cs
+++ ERHMS.Presentation/Converters/StringToUppercaseStringConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace ERHMS.Presentation.Converters
 {
-    public class StringToUpperConverter : IValueConverter
+    public class StringToUppercaseStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
