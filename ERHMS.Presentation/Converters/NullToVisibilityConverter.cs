c1aa0cbc17deb92abe4454dc8fbf1408b14f7572
Steven Williams <gev3@cdc.gov>
2016-05-16 06:23:03 -0400

    Hide menus before data source selection

--- ERHMS.Presentation/Converters/NullToVisibilityConverter.cs
+++ ERHMS.Presentation/Converters/NullToVisibilityConverter.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace ERHMS.Presentation.Converters
 {
-    public class NullToBoolConverter : IValueConverter
+    public class NullToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value != null;
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
