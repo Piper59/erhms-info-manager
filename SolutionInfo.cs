b4042fffb12386292bf6b9b4382c8ffdf84c1724
Steven Williams <gev3@cdc.gov>
2016-05-26 16:54:13 -0400

    Bump to version 0.5.1

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ERHMS Info Manager")]
 [assembly: AssemblyCompany("NIOSH")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("May 23, 2016")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyInformationalVersion("May 26, 2016")]
 [assembly: ComVisible(false)]
