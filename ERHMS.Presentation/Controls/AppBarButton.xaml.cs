87426404c6d11332e3341abf3ee8489259a7e413
Steven Williams <gev3@cdc.gov>
2016-07-12 15:25:55 -0400

    Rework app bar buttons

---
+++ ERHMS.Presentation/Controls/AppBarButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ERHMS.Presentation.Controls
+{
+    public partial class AppBarButton : Button
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon",
+            typeof(Visual),
+            typeof(AppBarButton));
+        public Visual Icon
+        {
+            get { return (Visual)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public AppBarButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
