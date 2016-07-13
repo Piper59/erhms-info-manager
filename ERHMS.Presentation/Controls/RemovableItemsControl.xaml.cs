d04f279c6f277a5f202bcb2336afe88c218d7090
Steven Williams <gev3@cdc.gov>
2016-07-13 10:47:37 -0400

    Allow middle-click on removable items

---
+++ ERHMS.Presentation/Controls/RemovableItemsControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace ERHMS.Presentation.Controls
+{
+    public partial class RemovableItemsControl : ItemsControl
+    {
+        public static readonly DependencyProperty ItemTextTemplateProperty = DependencyProperty.Register(
+            "ItemTextTemplate",
+            typeof(DataTemplate),
+            typeof(RemovableItemsControl));
+        public DataTemplate ItemTextTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTextTemplateProperty); }
+            set { SetValue(ItemTextTemplateProperty, value); }
+        }
+
+        public RemovableItemsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                FrameworkElement source = (FrameworkElement)e.Source;
+                FrameworkElement parent = (FrameworkElement)source.TemplatedParent;
+                Button button = (Button)Root.ItemTemplate.FindName("Remove", parent);
+                ICommand command = (ICommand)button.GetValue(ButtonBase.CommandProperty);
+                if (command != null && command.CanExecute(null))
+                {
+                    command.Execute(null);
+                }
+            }
+        }
+    }
+}
