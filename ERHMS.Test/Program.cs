883380796d03ff83befa6b5369809a97a76fa43f
Steven Williams <gev3@cdc.gov>
2017-01-27 14:38:16 -0500

    Add test project

---
+++ ERHMS.Test/Program.cs
@@ -0,0 +1,12 @@
+using NUnitLite;
+
+namespace ERHMS.Test
+{
+    public class Program
+    {
+        internal static int Main(string[] args)
+        {
+            return new AutoRun().Execute(args);
+        }
+    }
+}
