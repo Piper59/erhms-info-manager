650f30922d73722ee05d30def1ff94ee6124deeb
Steven Williams <gev3@cdc.gov>
2017-09-08 13:42:47 -0400

    Bump beta to version 2.1.0

--- ERHMS.Beta/Properties/AssemblyInfo.cs
+++ ERHMS.Beta/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ERHMS.Beta")]
 [assembly: AssemblyProduct("ERHMS Info Manager Beta")]
 [assembly: AssemblyCompany("NIOSH")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("August 29, 2017")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyInformationalVersion("September 8, 2017")]
 [assembly: ComVisible(false)]
