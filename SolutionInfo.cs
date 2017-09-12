dd7593803cbb237499fa2e49b73f6860e5dc28b3
Steven Williams <gev3@cdc.gov>
2017-09-12 16:39:27 -0400

    Bump beta to version 2.1.1

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ERHMS Info Manager")]
 [assembly: AssemblyCompany("NIOSH")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("August 29, 2017")]
+[assembly: AssemblyInformationalVersion("Beta")]
 [assembly: ComVisible(false)]
