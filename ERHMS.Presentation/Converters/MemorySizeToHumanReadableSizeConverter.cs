a44309beb2326464a7ca2accfc882efa1e668989
Steven Williams <gev3@cdc.gov>
2017-12-12 13:55:18 -0500

    Clean up converters

--- ERHMS.Presentation/Converters/MemorySizeToHumanReadableSizeConverter.cs
+++ ERHMS.Presentation/Converters/MemorySizeToHumanReadableSizeConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 
@@ -8,27 +9,21 @@ namespace ERHMS.Presentation.Converters
 {
     public class MemorySizeToHumanReadableSizeConverter : IValueConverter
     {
-        private static readonly IList<long> Sizes = new long[]
+        private static readonly IDictionary<long, string> Units = new Dictionary<long, string>
         {
-            1L << 10,
-            1L << 20,
-            1L << 30
-        };
-        private static readonly IList<string> Units = new string[]
-        {
-            "KB",
-            "MB",
-            "GB"
+            { 1L << 10, "KB" },
+            { 1L << 20, "MB" },
+            { 1L << 30, "GB" }
         };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             long size = (long)value;
-            for (int index = Units.Count - 1; index >= 0; index--)
+            foreach (KeyValuePair<long, string> unit in Units.OrderByDescending(unit => unit.Key))
             {
-                if (size > Sizes[index])
+                if (size > unit.Key)
                 {
-                    return string.Format("{0:n1} {1}", (double)size / Sizes[index], Units[index]);
+                    return string.Format("{0:n1} {1}", (double)size / unit.Key, unit.Value);
                 }
             }
             return string.Format("{0} B", size);
