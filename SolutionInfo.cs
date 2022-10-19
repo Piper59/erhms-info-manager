3b7fcbb13eade3b513fe1082160775ee04fe4920
Morton, Mark <ojh4@cdc.gov>
2022-10-19 08:55:23 -0400

    3.0.0.4  Latest Release

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ERHMS Info Manager")]
 [assembly: AssemblyCompany("NIOSH")]
-[assembly: AssemblyVersion("3.0.0.3")]
-[assembly: AssemblyInformationalVersion("3.0 RC3")]
+[assembly: AssemblyVersion("3.0.0.4")]
+[assembly: AssemblyInformationalVersion("3.0")]
 [assembly: AssemblyCopyright("\u00a9 2022 National Institute for Occupational Safety and Health")]
 [assembly: ComVisible(false)]
